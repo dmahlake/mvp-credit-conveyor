c170716a2163f258b1fc109f4ff79f6411e4377d
dipuo <dmahlake@student.wethinkcode.com>
2022-10-24 21:06:47 +0200

    skeleton

---
+++ src/main/java/com/mvplevel1/Mvplevel1Application.java
@@ -0,0 +1,13 @@
+package com.mvplevel1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvplevel1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvplevel1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvplevel1/Mvplevel1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvplevel1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvplevel1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
